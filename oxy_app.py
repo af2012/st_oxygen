536664ee96e42aa60a3201d72d58a8cfd0df84c5
af2012 <af2012@gmail.com>
2024-08-20 13:16:59 -0400

    Update oxy_app.py

--- oxy_app.py
+++ oxy_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
 def calculate_remaining_time(psi, flow_rate):
-    remaining_time = (0.28 * (psi - 500)) / flow_rate
+    remaining_time = (0.28 * (psi)) / flow_rate
     return remaining_time
 
 def main():
@@ -12,11 +12,11 @@ def main():
     flow_rate = st.number_input("Enter Flow Rate:", min_value=0)
     
     if st.button("Calculate Remaining Time"):
-        if psi >= 500:
+        if psi >= 0:
             remaining_time = calculate_remaining_time(psi, flow_rate)
             st.write(f"Remaining Time: {remaining_time:.2f} minutes")
         else:
-            st.error("PSI must be greater than or equal to 500")
+            st.error("PSI must be greater than or equal to 0")
 
 if __name__ == "__main__":
     main()
